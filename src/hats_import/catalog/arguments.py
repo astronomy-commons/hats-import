40ec73fd3fa14f56204ff2f39e08d45b17df912d
Sean McGuire <123987820+smcguire-cmu@users.noreply.github.com>
2025-04-15 15:04:04 -0400

    Update src/hats_import/catalog/arguments.py
    Co-authored-by: Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>

--- src/hats_import/catalog/arguments.py
+++ src/hats_import/catalog/arguments.py
@@ -13,7 +13,7 @@ from hats.io.paths import DATASET_DIR, PARTITION_ORDER
 from hats.pixel_math import spatial_index
 from upath import UPath
 
-from hats_import.catalog.file_readers import InputReader, ParquetReader, get_file_reader
+from hats_import.catalog.file_readers import InputReader, ParquetPyarrowReader, get_file_reader
 from hats_import.runtime_arguments import RuntimeArguments, find_input_paths
 
 # pylint: disable=too-many-locals,too-many-arguments,too-many-instance-attributes,too-many-branches,too-few-public-methods
