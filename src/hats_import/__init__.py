4f286c925326864f49fbf286c2d5e81950976f6e
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2025-09-04 16:07:24 -0400

    isort

--- src/hats_import/__init__.py
+++ src/hats_import/__init__.py
@@ -3,9 +3,9 @@
 from ._version import __version__
 from .catalog import ImportArguments
 from .collection import CollectionArguments
+from .hipscat_conversion import ConversionArguments
 from .index import IndexArguments
 from .margin_cache.margin_cache_arguments import MarginCacheArguments
 from .pipeline import pipeline, pipeline_with_client
 from .runtime_arguments import RuntimeArguments
 from .verification.arguments import VerificationArguments
-from .hipscat_conversion import ConversionArguments
