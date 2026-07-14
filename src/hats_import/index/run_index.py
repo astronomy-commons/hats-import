e1afc2af8b1cdf9b4768b376e87104f434f3b04f
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2026-07-14 10:49:30 -0400

    Generate additional static files. Test against full dependencies. (#698)
    * Generate additional static files. Test against full dependencies.
    
    * Add some comments in smoke test file

--- src/hats_import/index/run_index.py
+++ src/hats_import/index/run_index.py
@@ -1,6 +1,7 @@
 """Create columnar index of hats table using dask for parallelization"""
 
 from hats.io import file_io, parquet_metadata
+from hats.io.summary_file import write_catalog_summary_file
 
 import hats_import.index.map_reduce as mr
 from hats_import.index.arguments import IndexArguments
@@ -18,17 +19,25 @@ def run(args, client):
 
     # All done - write out the metadata
     with print_progress(
-        total=3,
+        total=5,
         stage_name="Finishing",
         pipeline_name="index",
         use_progress_bar=args.progress_bar,
         simple_progress_bar=args.simple_progress_bar,
         tqdm_kwargs=args.tqdm_kwargs,
     ) as step_progress:
+        # pylint: disable=duplicate-code
         index_catalog_info = args.to_table_properties(int(rows_written))
         index_catalog_info.to_properties_file(args.catalog_path)
         step_progress.update(1)
         file_io.remove_directory(args.tmp_path, ignore_errors=True)
         step_progress.update(1)
         parquet_metadata.write_parquet_metadata(args.catalog_path, order_by_healpix=False)
+        step_progress.update(1)
+
+        if args.create_summary_html:
+            write_catalog_summary_file(args.catalog_path, fmt="html")
+        step_progress.update(1)
+        if args.create_summary_md:
+            write_catalog_summary_file(args.catalog_path, fmt="markdown")
         step_progress.update(1)
